a6f3b1439cdfdf725ef1a03149da4a518c1492f4
Sudhakar <sudhakar.s@rently.com>
2021-12-17 12:03:23 +0530

    Calling function from another file

---
+++ Callbacks & Promises/function_call/file_A.js
@@ -0,0 +1,7 @@
+const {name,fun}=require("./file_B");
+
+console.log(name);
+
+fun("This is the message");
+
+
---
+++ Callbacks & Promises/function_call/file_B.js
@@ -0,0 +1,16 @@
+
+console.log(__filename);
+
+console.log(__dirname);
+
+var name="File_name";
+
+function fun(msg)
+{
+	console.log(msg);
+}
+
+module.exports.name=name;
+
+module.exports.fun=fun;
+
